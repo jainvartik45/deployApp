3a9dea5d1c490b4789b42b98c68c56df4c9eea37
jainvartik45 <84932356+jainvartik45@users.noreply.github.com>
2023-05-04 13:37:38 +0530

    9

--- index.js
+++ index.js
@@ -10,14 +10,12 @@ import path from 'path'
 
 
 const app = express();
-// const __dirname = path.resolve(path.dirname('')); 
+const __dirname = path.resolve(path.dirname('')); 
 
 app.use(express.static(path.join(__dirname,'./build')))
 
 
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./build/index.html"));
-});
+
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
@@ -25,6 +23,10 @@ app.use(cors());
 
 app.use('/posts',postRouter);
 app.use('/user' , userRouter)
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./build/index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 
